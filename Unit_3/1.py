6ada2e5f4242c9d1ab9367197e8da1dfa6cab801
kunjalmaheta17 <mahetakunjal2@gmail.com>
2026-08-12 12:59:04 +0530

    Add files via upload

---
+++ Unit_3/1.py
@@ -0,0 +1,5 @@
+#  Write a program to create and import a userdefined module.
+
+import module_1 as m
+
+print("Module imported")
---
+++ Unit_3/2.py
@@ -0,0 +1,29 @@
+#Write a program to demonstrate different import mechanisms in Python.
+
+# 1. Import complete module
+import module_1
+
+print("- Using import module:")
+print("Addition:", module_1.add(10, 5))
+
+# 2. Import specific function
+from module_1 import add
+
+print("\n- import function:")
+print("Addition:", add(20, 10))
+
+
+# 3. Import multiple functions
+from module_1 import add, multiply
+
+print("\n- import multiple functions:")
+print("Addition:", add(30, 10))
+print("Multiplication:", multiply(30, 10))
+
+
+# 4. Import module with alias
+import module_1 as m
+
+print("\n- as alias:")
+print("Addition:", m.add(40, 10))
+print("Multiplication:", m.multiply(40, 10))
